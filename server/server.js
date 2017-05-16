3e5569d86efc0a65e9bba8c2d154cb6b7ea518c2
Ralph L <lettau.ralph@gmail.com>
2017-05-16 16:33:44 +0200

    Added emit and brodacasting

--- server/server.js
+++ server/server.js
@@ -13,14 +13,29 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',  {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
 
-    socket.on('createMessage', (newMessage) => {
-        console.log('Created Message', newMessage);
+    socket.on('createMessage', (message) => {
+        console.log('Created Message', message);
         io.emit('newMessage', {
             from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().getTime()
-        })
+        }); //everybody gets it, even sender
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // }); // everybody but gets but sender
     })
 
     socket.on('disconnect', () => {
